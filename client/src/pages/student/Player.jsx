996a48ed74138020530b1ef6991658b798de15b3
sakibdeveloper999 <arjunsakib43@gmail.com>
2026-09-04 15:24:43 +0600

    feat: implement course data retrieval and display in Player component

--- client/src/pages/student/Player.jsx
+++ client/src/pages/student/Player.jsx
@@ -1,8 +1,84 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { AppContext } from '../../context/AppContext';
+import { useParams } from 'react-router-dom';
 
-const Player=()=>{
+const Player = () => {
+  const {enrolledCourses, calculateChapterTime} = useContext(AppContext)
+  const {courseId} = useParams()
+  const [courseData, setCoursData] = useState(null)
+  const [openSections, setOpenSections] = useState({})
+  const [playerData, setPlayerData] = useState(null)
+
+  const getCourseData =()=>{
+    enrolledCourses.map((course)=>{
+      if(course._id === courseId){
+        setCoursData(course)
+      }
+    })
+  }
+
+ const toggleSection = (index) => {
+    setOpenSections((prevState) => ({
+      ...prevState,
+      [index]: !prevState[index],
+    }));
+  }
+
+  useEffect(()=>{
+    getCourseData()
+  },[])
+
   return (
-    <div>Player</div>
+    <>
+      <div className='p-4 sm:p-10 flex flex-col-reverse md:grid md:grid-cols-2 gap-10 md:px-36'>
+
+        {/* left column  */}
+        <div className='text-gray-800'>
+          <h2 className='text-xl font-samibold'>Course Structure</h2>
+          <div className="pt-5">
+            { courseData && courseData.courseContent.map((chapter, index) => (
+              <div key={index} className='border border-gray-300 bg-white mb-2 rounded'>
+                <div className='flex items-center justify-between px-4 py-3 cursor-pointer select-none' onClick={() => toggleSection(index)}>
+                  <div className='flex items-center gap-2'>
+                    <img className={`transition-transform duration-300 ${openSections[index] ? 'rotate-180' : ''}`}
+                      src={assets.down_arrow_icon} alt="down_arrow_icon" />
+                    <p className='font-medium md:text-base text-sm'>{chapter.chapterTitle}</p>
+                  </div>
+                  <p className='text-sm md:text-default'>{chapter.chapterContent.length} lectures - {calculateChapterTime(chapter)}</p>
+                </div>
+
+                <div className={`overflow-hidden transition-all duration-300 ${openSections[index] ? 'max-h-96' : 'max-h-0'}`}>
+                  <ul className='list-disc md:pl-10 pl-4 pr-4 py-2 text-gray-600 border-t border-gray-300 '>
+                    {chapter.chapterContent.map((lecture, idx) => (
+                      <li key={idx} className='flex items-start gap-2 py-1'>
+                        <img src={false ? assets.play_icon : assets.pause_icon} alt="play_icon" className='w-4 h-4 mt-1' />
+                        <div className='flex items-center justify-between w-full text-gray-800 text-xs md:text-default'>
+                          <p className=''>{lecture.lectureTitle}</p>
+                          <div className='flex gap-2'>
+                            {lecture.isPreviewFree && <p
+                              onClick={() => setPlayerData({
+                                videoId: lecture.lectureUrl.split('/').pop(),
+                                title: lecture.lectureTitle
+                              })}
+                              className='text-blue-500 cursor-pointer'>Preview</p>}
+                            <p>{humanizeDuration(lecture.lectureDuration * 60 * 1000, { units: ['h', 'm'] })}</p>
+                          </div>
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+
+              </div>
+            ))}
+          </div>
+        </div>
+        {/* right column */}
+         <div>
+
+        </div>
+      </div>
+    </>
   )
 }
 
