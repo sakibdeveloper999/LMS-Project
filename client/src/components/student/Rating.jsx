1090aa737e61c9358f960e73700175b1762e2d07
sakibdeveloper999 <arjunsakib43@gmail.com>
2026-09-05 19:32:42 +0600

    feat: update Player component with Rating and YouTube integration, enhance course content display

--- client/src/components/student/Rating.jsx
+++ client/src/components/student/Rating.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
-const Rating=()=>{
+const Rating = ({ initialRating = 0, onRatingChange }) => {
+  const [rating, setRating] = useState(initialRating);
+
+  const handleRating = (value) => {
+    setRating(value);
+
+    if (onRatingChange) {
+      onRatingChange(value);
+    }
+  };
+
+  useEffect(() => {
+    setRating(initialRating);
+  }, [initialRating]);
+
   return (
-    <div>Rating</div>
-  )
-}
+    <div className="flex items-center gap-1">
+      {Array.from({ length: 5 }, (_, index) => {
+        const starValue = index + 1;
 
-export default Rating
+        return (
+          <span
+            key={index}
+            className={`text-xl sm:text-2xl cursor-pointer transition-colors ${
+              starValue <= rating
+                ? "text-yellow-500"
+                : "text-gray-400"
+            }`}
+            onClick={() => handleRating(starValue)}
+          >
+            &#9733;
+          </span>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Rating;
--- client/src/pages/student/Player.jsx
+++ client/src/pages/student/Player.jsx
@@ -1,6 +1,12 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { AppContext } from '../../context/AppContext';
 import { useParams } from 'react-router-dom';
+import {assets} from '../../assets/assets';
+import humanizeDuration from 'humanize-duration';
+import YouTube from 'react-youtube';
+import Footer from '../../components/student/Footer';
+import Rating from '../../components/student/Rating';
+
 
 const Player = () => {
   const {enrolledCourses, calculateChapterTime} = useContext(AppContext)
@@ -38,7 +44,7 @@ const Player = () => {
           <div className="pt-5">
             { courseData && courseData.courseContent.map((chapter, index) => (
               <div key={index} className='border border-gray-300 bg-white mb-2 rounded'>
-                <div className='flex items-center justify-between px-4 py-3 cursor-pointer select-none' onClick={() => toggleSection(index)}>
+                <div className='flex items-center justify-between px-4 py-3 cursor-pointer select-none ' onClick={() => toggleSection(index)}>
                   <div className='flex items-center gap-2'>
                     <img className={`transition-transform duration-300 ${openSections[index] ? 'rotate-180' : ''}`}
                       src={assets.down_arrow_icon} alt="down_arrow_icon" />
@@ -51,16 +57,15 @@ const Player = () => {
                   <ul className='list-disc md:pl-10 pl-4 pr-4 py-2 text-gray-600 border-t border-gray-300 '>
                     {chapter.chapterContent.map((lecture, idx) => (
                       <li key={idx} className='flex items-start gap-2 py-1'>
-                        <img src={false ? assets.play_icon : assets.pause_icon} alt="play_icon" className='w-4 h-4 mt-1' />
+                        <img src={false ? assets.blue_tick_icon : assets.play_icon} alt="play_icon" className='w-4 h-4 mt-1' />
                         <div className='flex items-center justify-between w-full text-gray-800 text-xs md:text-default'>
                           <p className=''>{lecture.lectureTitle}</p>
                           <div className='flex gap-2'>
-                            {lecture.isPreviewFree && <p
+                            {lecture.lectureUrl && <p
                               onClick={() => setPlayerData({
-                                videoId: lecture.lectureUrl.split('/').pop(),
-                                title: lecture.lectureTitle
+                                ...lecture, chapter: index + 1, lecture: idx + 1
                               })}
-                              className='text-blue-500 cursor-pointer'>Preview</p>}
+                              className='text-blue-500 cursor-pointer'>Watch</p>}
                             <p>{humanizeDuration(lecture.lectureDuration * 60 * 1000, { units: ['h', 'm'] })}</p>
                           </div>
                         </div>
@@ -72,12 +77,33 @@ const Player = () => {
               </div>
             ))}
           </div>
-        </div>
-        {/* right column */}
-         <div>
+
+          {/* Rating Component:  */}
+          <div className="flex items-center gap-3 py-3 mt-10">
+            <h1 className="text-xl font-bold">Rate this Course:</h1>
+            <Rating initialRating={0} />
+          </div>
 
         </div>
+        {/* right column */}
+         <div className='md:mt-10 '>
+          {playerData ? (
+            <div className='w-full h-64 md:h-96'>
+              <YouTube
+                videoId={playerData.lectureUrl.split('/').pop()}
+                iframeClassName='w-full aspect-video'
+              />
+              <div className="flex justify-between items-center mt-1">
+                <p>{playerData.chapter}.{playerData.lecture} {playerData.lectureTitle}</p>
+                <button className="bg-blue-500 text-white py-1 px-3 rounded-md hover:bg-blue-600">{false ? 'Completed': 'Mark Complete'}</button>
+              </div>
+            </div>
+          ) : (
+            <img src={courseData ? courseData.courseThumbnail : ''} alt="" />
+          )}
+        </div>
       </div>
+      <Footer/>
     </>
   )
 }
