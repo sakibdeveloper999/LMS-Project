10fe4d3cb88b49c771db60610c68ca986ad5a6da
sakibdeveloper999 <arjunsakib43@gmail.com>
2026-09-05 22:39:45 +0600

    feat: implement Navbar component for educator page with user greeting and logo

--- client/src/components/educator/Navbar.jsx
+++ client/src/components/educator/Navbar.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { assets ,dummyEducatorData } from '../../assets/assets' 
+import {UserButton, useUser} from '@clerk/clerk-react'
+import { Link } from 'react-router-dom'
 
 const Navbar=()=>{
+  const { user } = useUser()
+  const educatorData = dummyEducatorData
   return (
-    <div>Edu Navbar</div>
+    <div className='flex items-center justify-between px-4 md:px-8 py-3 border-b border-gray-500'>
+      <Link to='/'>
+       <img src={assets.logo} alt="logo" className='w-28 lg:w-32' />
+      </Link>
+      <div className="flex items-start gap-5 text-gray-500 relative">
+        <p>Hi! {user ? user.fullName : 'Developers'}</p>
+        {user ? <UserButton/> : <img className='max-w-8' src={assets.profile_img} />}
+      </div>
+    </div>
   )
 }
 
--- client/src/pages/educator/Educator.jsx
+++ client/src/pages/educator/Educator.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Navbar from '../../components/educator/Navbar';
 
 const Educator=()=> {
   return (
     <div>
-        <h1>Eductor page</h1>
+        <Navbar/>
         <div>
             {<Outlet/>}
         </div>
