c9f933e5b5408b2d72eb219b31ec9a33af8c684e
sakibdeveloper999 <arjunsakib43@gmail.com>
2026-09-05 23:16:10 +0600

    feat: enhance Sidebar component for educator page with dynamic menu and context integration

--- client/src/components/educator/Sidebar.jsx
+++ client/src/components/educator/Sidebar.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import { assets } from '../../assets/assets';
+import { useContext } from 'react';
+import { NavLink } from 'react-router-dom';
+import {AppContext} from '../../context/AppContext'
 
 const Sidebar=()=>{
-  return (
-    <div>Sidebar</div>
+
+  const {isEducator} = useContext(AppContext);
+
+  const menuItems = [
+    { name: 'Dashboard', path: '/educator', icon: assets.home_icon },
+    { name: 'Add Course', path: '/educator/add-course', icon: assets.add_icon },
+    { name: 'My Courses', path: '/educator/my-courses', icon: assets.my_course_icon },
+    { name: 'Student Enrolled', path: '/educator/student-enrolled', icon: assets.person_tick_icon },
+  ];
+
+
+  return isEducator && (
+     <div className="md:w-64 w-16 border-r min-h-screen text-base border-gray-500 py-2 flex flex-col">
+      {menuItems.map((item) => (
+        <NavLink
+         to={item.path}
+         key={item.name}
+         end={item.path === '/educator'}
+         className={({ isActive }) =>
+           `flex items-center md:flex-row flex-col md:justify-start justify-center py-3.5 md:px-10 gap-3 ${isActive ? 'bg-gray-200' : ''}`
+         }
+        >
+          <img src={item.icon} alt='icon' className='w-6 h-6' />
+          <p className='md:block hidden text-center'>{item.name}</p>
+        </NavLink>
+      ))}
+     </div>
   )
 }
 
--- client/src/pages/educator/Educator.jsx
+++ client/src/pages/educator/Educator.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from '../../components/educator/Navbar';
+import Sidebar from '../../components/educator/Sidebar';
+
 
 const Educator=()=> {
   return (
-    <div>
+    <div clessName="text-defult min-h-screen bg-white">
         <Navbar/>
-        <div>
-            {<Outlet/>}
+        <div className='flex'>
+            <Sidebar/>
+            <div className='flex-1'>
+                {<Outlet/>}
+            </div>
         </div>
     </div>
   )
