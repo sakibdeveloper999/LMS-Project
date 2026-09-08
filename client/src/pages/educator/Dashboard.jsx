782b95127616b583496b03aa146466ca7c5efa79
sakibdeveloper999 <arjunsakib43@gmail.com>
2026-09-08 22:00:42 +0600

    feat: update Dashboard component to fetch and display dashboard data with loading state

--- client/src/pages/educator/Dashboard.jsx
+++ client/src/pages/educator/Dashboard.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { use, useContext, useEffect } from 'react'
+import { AppContext } from '../../context/AppContext';
+import { assets, dummyDashboardData } from '../../assets/assets';
+import Loading from '../../components/student/Loading';
 
 const Dashboard=()=> {
-  return (
-    <div>Dashboard</div>
-  )
+
+  const {currency} = useContext(AppContext) 
+  const [dashboardData, setDashboardData] = useState(null)
+
+  const fetchDashboardData = async ()=>{
+    setDashboardData(dummyDashboardData)
+  }
+
+  useEffect(() => {
+    fetchDashboardData()
+  }, [])
+  
+
+  return dashboardData ?(
+    <div className='min-h-screen flex flex-col items-start justify-between gap-8 md:p-8 p-4 pt-8 pb-0'>
+      <div className="space-y-5">
+        <div className="flex flex-wrap gap-5 items-center ">
+          {/* =========== ONE ==================  */}
+          <div className="flex items-center gap-3 shadow-card border border-blue-500 p-4 w-56 rounded-md">
+            <img src={assets.patients_icon} alt="patiensts_icon" />
+            <div >
+              <p className="text-2xl font-medium text-gray-600 ">{dashboardData.enrolledStudentsData.length}</p>
+              <p className='text-base text-gray-500'>Total Enrolments</p>
+            </div>
+          </div>
+          {/* =========== TOW ==================  */}
+          <div className="flex items-center gap-3 shadow-card border border-blue-500 p-4 w-56 rounded-md">
+            <img src={assets.appointments_icon} alt="appointments_icon" />
+            <div >
+              <p className="text-2xl font-medium text-gray-600 ">{dashboardData.totalCourses}</p>
+              <p className='text-base text-gray-500'>Total Courses</p>
+            </div>
+          </div>
+          {/* =========== THREE ==================  */}
+          <div className="flex items-center gap-3 shadow-card border border-blue-500 p-4 w-56 rounded-md">
+            <img src={assets.earning_icon} alt="earnings_icon" />
+            <div >
+              <p className="text-2xl font-medium text-gray-600 ">{dashboardData.totalEarings}</p>
+              <p className='text-base text-gray-500'>Total Earnings</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  ) : <Loading/>
 }
 
 export default Dashboard
